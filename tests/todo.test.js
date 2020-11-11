6e17c9e84cc12586f61a6334cb2275d35ac329b7
jssarrazolaa <jssarrazolaa@unal.edu.co>
2020-11-11 13:15:43 -0500

    Fix linters

--- tests/todo.test.js
+++ tests/todo.test.js
@@ -1,6 +1,4 @@
-
 import Todo from '../src/todo';
-
 
 const todo = new Todo('Todo Test', 'Testing', '2020-11-06', 0, true);
 
@@ -13,17 +11,17 @@ it('checks for todo title', () => {
 });
 
 it('checks for todo description', () => {
-    expect(todo.description).toBe('Testing');
+  expect(todo.description).toBe('Testing');
 });
 
 it('checks for todo date', () => {
-    expect(todo.date).toBe('2020-11-06');
+  expect(todo.date).toBe('2020-11-06');
 });
 
 it('checks for todo priority', () => {
-    expect(todo.priotiry).toBe(0);
+  expect(todo.priotiry).toBe(0);
 });
 
 it('checks for todo date', () => {
-    expect(todo.done).toBe(true);
+  expect(todo.done).toBe(true);
 });
