5fb70b5e03d03eaf8edeca4049565c62cc365aab
jssarrazolaa <jssarrazolaa@unal.edu.co>
2020-11-11 11:48:30 -0500

    Create test files

---
+++ tests/edit.test.js
@@ -0,0 +1,12 @@
+/* import update from '../src/edit';
+import createProject from '../src/createProject';
+
+document.body.innerHtml = "<div id='projectsContainer'><div>Title</div></div>";
+
+/* const projectsCont = document.getElementById('projectsContainer');
+createProject(JSON.parse(localStorage.getItem('myTodos')));
+const myTodos = JSON.parse(localStorage.getItem('myTodos')); */
+
+it('checks for the update ', () => {
+
+});
